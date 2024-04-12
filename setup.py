0dcea202a47918507edebb9e80f8c70ea9d618ad
Maria-Djadi01 <maria.djadi01@gmail.com>
2024-04-12 21:53:07 +0100

    Set project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains the implementation of two specialized AI chatbots developed to address challenges in the pharmaceutical and healthcare sector and regulatory compliance landscape.',
+    author='Maria Djadi | Dounia Toubal',
+    license='',
+)
